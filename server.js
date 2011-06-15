b69ca01e3d84fff43dc4fa215633b14efd2551ab
Florian Traverse <florian.traverse@gmail.com>
2011-06-15 15:58:05 +0200

    readme improvment

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var connect = require('connect');
 var optimist = require('optimist');
 var argv = optimist
     .usage('Serve the slides.\nUsage: $0')
+    
     .demand('p')
     .alias('p', 'port')
     .default({p: 3000})    
     .describe('p', 'Set server port')
+    
     .boolean('h')
     .alias('h','help')
     .describe('h', "Print help")
+    
     .argv
 ;
 if(argv.h) return optimist.showHelp();
