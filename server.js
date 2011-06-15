f78b0aa21944905615b78f405a719350a89c10b4
Florian Traverse <florian.traverse@gmail.com>
2011-06-15 15:50:40 +0200

    added unuseful commandline parsing for launching static server

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 var connect = require('connect');
+var optimist = require('optimist');
+var argv = optimist
+    .usage('Serve the slides.\nUsage: $0')
+    .demand('p')
+    .alias('p', 'port')
+    .default({p: 3000})    
+    .describe('p', 'Set server port')
+    .boolean('h')
+    .alias('h','help')
+    .describe('h', "Print help")
+    .argv
+;
+if(argv.h) return optimist.showHelp();
 
 var server = connect(
 	connect.logger()
   , connect.static(__dirname)
 );
 
-server.listen(3000)
+server.listen(argv.p);
